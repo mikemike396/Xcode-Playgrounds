eb66f97abe394de633198e25357fe55d5075da8c
Michael Kushinski <mikekushinski@gmail.com>
2022-03-25 12:25:05 -0400

    Added Async await getImages loaded into a Scrollable VGrid

---
+++ AsyncAwait_Scrollable_VGrid_Demo.playground/Contents.swift
@@ -0,0 +1,61 @@
+import Foundation
+import _Concurrency
+import SwiftUI
+import PlaygroundSupport
+
+struct Model: Decodable, Hashable {
+    let url: URL
+}
+
+class ViewModel: ObservableObject {
+    let apiURL = "https://jsonplaceholder.typicode.com/photos"
+    @Published var images: [Model] = []
+    
+    func loadData() async {
+        do {
+            guard let url = URL(string: apiURL) else { return }
+            images = try await getImageUrls(from: url)
+        } catch {
+            // Handle Error
+        }
+    }
+    
+    func getImageUrls(from url: URL) async throws -> [Model] {
+        let (data, response) = try await URLSession.shared.data(from: url)
+        let results = try JSONDecoder().decode([Model].self, from: data)
+        return results
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]) {
+                ForEach(viewModel.images.shuffled().prefix(20), id: \.self) { image in
+                    AsyncImage(
+                        url: image.url,
+                        content: { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        },
+                        placeholder: {
+                            ProgressView()
+                        }
+                    )
+                }
+            }
+        }
+        .task {
+            await viewModel.loadData()
+        }
+    }
+}
+
+
+let view = ContentView()
+let viewModel = ViewModel()
+let hostingVC = UIHostingController(rootView: view)
+PlaygroundPage.current.liveView = hostingVC
